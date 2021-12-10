107efa0a9f40e1c36abf1a926727381391458931
CESSIDY <vrupa1713@gmail.com>
2021-12-10 14:19:00 +0100

    - feat(useragent_middleware): added mode logic;

--- src/python/src/middlewares/user_agent_middleware.py
+++ src/python/src/middlewares/user_agent_middleware.py
@@ -1,19 +1,29 @@
 from scrapy import Spider, Request, Spider, signals
 import logging
 from datetime import datetime, timedelta
+import random
+import json
+
+log = logging.getLogger('scrapy.useragent')
+
+class Mode:
+    RANDOMIZE_ONCE, RANDOMIZE_EVERY_REQUESTS, IN_ORDER_EVERY_REQUESTS = range(1,4) # 1, 2 ,3
 
 class UserAgentMiddleware:
     """This middleware allows spiders to use the user_agent from list"""
 
-    def __init__(self) -> None:
-        self.user_agents = None
+    def __init__(self, *args, **kwargs):
+        self.last_user_agent = None
         self.user_agent = None
+        self.user_agents = None
 
     @classmethod
     def from_crawler(cls, crawler):
         o = cls()
         o.user_agent_release_data_checing(crawler)
         o.user_agent = crawler.settings['USER_AGENT']
+        o.user_agents = crawler.settings['USER_AGENT_LIST']
+        o.mode = crawler.settings['USER_AGENT_MODE']
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
         return o
 
@@ -21,11 +31,11 @@ class UserAgentMiddleware:
     def user_agent_release_data_checing(crawler):
         user_agent_release_date = crawler.settings['USER_AGENT_RELEASE_DATE']
         if datetime(*[int(number) for number in user_agent_release_date.split('-')]) + timedelta(days=180) < datetime.now():
-            logging.warning('USER_AGENT is outdated')
+            log.warning('USER_AGENT is outdated')
 
     def spider_opened(self, spider: Spider):
-        logging.info("Spider Open")
         self.user_agents = getattr(spider, 'user_agents', self.user_agents)
+        self.mode = getattr(spider, 'user_agent_mode', self.mode)
 
     def process_request(self, request: Request, spider: Spider):
         if self.user_agents:
@@ -34,6 +44,16 @@ class UserAgentMiddleware:
             request.headers.setdefault(b'User-Agent', self.user_agent)
 
     def get_user_agent_from_list(self):
-        user_agent = self.user_agents.pop(0)
-        self.user_agents.append(user_agent)
+        if self.mode == Mode.RANDOMIZE_ONCE:
+            if not self.last_user_agent:
+                user_agent = random.choice(list(self.user_agents))
+                self.last_user_agent = user_agent
+            else:
+                user_agent = self.last_user_agent
+        elif self.mode == Mode.RANDOMIZE_EVERY_REQUESTS:
+            user_agent = random.choice(list(self.user_agents))
+        elif self.mode == Mode.IN_ORDER_EVERY_REQUESTS:
+            user_agent = self.user_agents.pop(0)
+            self.user_agents.append(user_agent)
+        log.info(user_agent)
         return str(user_agent).strip()
--- src/python/src/spiders/test_spider.py
+++ src/python/src/spiders/test_spider.py
@@ -12,17 +12,6 @@ class TestSpider(Spider):
         }
     }
 
-    user_agents = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
-        "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1"
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-        "Mozilla/5.0 (Linux; Android 7.0; SM-T827R4 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.116 Safari/537.36",
-        "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.24 Safari/537.36",
-        ]
-
     def parse(self, response, **kwargs):
         self.logger.info(response.request.headers['User-Agent'])
         if self.index < 10:
