a9f00f6b9d39faff33ea4b80d67a111fdafe5863
CESSIDY <vrupa1713@gmail.com>
2021-12-22 16:15:38 +0100

    - feat(middleware): added user_agents_ setter and user_agents_iter;

--- src/python/src/middlewares/user_agent_middleware.py
+++ src/python/src/middlewares/user_agent_middleware.py
@@ -2,14 +2,16 @@ from scrapy import Spider, Request, Spider, signals
 import logging
 from datetime import datetime, timedelta
 import random
-import json
+import itertools
 
 log = logging.getLogger('scrapy.useragent')
+
 
 class UserAgentMode:
     RANDOMIZE_ONCE = 1
     RANDOMIZE_EVERY_REQUESTS = 2
     IN_ORDER_EVERY_REQUESTS = 3
+
 
 class UserAgentMiddleware:
     """
@@ -24,7 +26,7 @@ class UserAgentMiddleware:
         ]
         USER_AGENT_MODE = 1 or 2 or 3
     """
-
+    UA_RELEVAN_DAYS = 180
 
     def __init__(self, *args, **kwargs):
         self.last_user_agent = None
@@ -34,28 +36,41 @@ class UserAgentMiddleware:
     @classmethod
     def from_crawler(cls, crawler):
         o = cls()
-        o.user_agent_release_data_checing(crawler)
+        o.user_agent_release_data_checking(crawler)
         o.user_agent = crawler.settings['USER_AGENT']
-        o.user_agents = crawler.settings['USER_AGENT_LIST']
+        o._set_user_agents(crawler.settings['USER_AGENT_LIST'])
         o.mode = crawler.settings['USER_AGENT_MODE']
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
         return o
 
     @staticmethod
-    def user_agent_release_data_checing(crawler):
+    def user_agent_release_data_checking(crawler):
         user_agent_release_date = crawler.settings['USER_AGENT_RELEASE_DATE']
-        if datetime(*[int(number) for number in user_agent_release_date.split('-')]) + timedelta(days=180) < datetime.now():
+        if datetime(*[int(number) for number in user_agent_release_date.split('-')]) + timedelta(days=UserAgentMiddleware.UA_RELEVAN_DAYS) < datetime.now():
             log.warning('USER_AGENT is outdated')
 
+    def _set_user_agents(self, _user_agents):
+        if _user_agents:
+            if type(_user_agents) is list:
+                _user_agents = [str(user_agent).strip()
+                                for user_agent in _user_agents]
+                self.user_agents = _user_agents
+                self.user_agents_iter = itertools.cycle(_user_agents)
+            else:
+                log.warning("USER_AGENT_LIST is not a list")
+
     def spider_opened(self, spider: Spider):
-        self.user_agents = getattr(spider, 'user_agents', self.user_agents)
+        self._set_user_agents(getattr(spider, 'user_agents', self.user_agents))
         self.mode = getattr(spider, 'user_agent_mode', self.mode)
 
     def process_request(self, request: Request, spider: Spider):
         if self.user_agents:
-            request.headers.setdefault(b'User-Agent', self.get_user_agent_from_list())
+            request.headers.setdefault(
+                b'User-Agent', self.get_user_agent_from_list())
         elif self.user_agent:
             request.headers.setdefault(b'User-Agent', self.user_agent)
+        else:
+            log.error("USER_AGENT and USER_AGENT_LIST is empty!")
 
     def get_user_agent_from_list(self):
         if self.mode == UserAgentMode.RANDOMIZE_ONCE:
@@ -65,9 +80,8 @@ class UserAgentMiddleware:
             else:
                 user_agent = self.last_user_agent
         elif self.mode == UserAgentMode.RANDOMIZE_EVERY_REQUESTS:
-            user_agent = random.choice(list(self.user_agents))
+            user_agent = random.choice(self.user_agents)
         elif self.mode == UserAgentMode.IN_ORDER_EVERY_REQUESTS:
-            user_agent = self.user_agents.pop(0)
-            self.user_agents.append(user_agent)
+            user_agent = next(self.user_agents_iter)
         log.info(user_agent)
-        return str(user_agent).strip()
+        return user_agent
